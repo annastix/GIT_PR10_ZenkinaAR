71a447445ac7bbca673ce44f1e5d254c084f60c7
Zenkina <anastasia.lllll@yandex.ru>
2025-10-28 11:36:44 +0300

    Добавление ФИО и номера группы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy"));
+Console.WriteLine("Zenkina Anastasia Romanovna");
+Console.WriteLine("IS22-A");
